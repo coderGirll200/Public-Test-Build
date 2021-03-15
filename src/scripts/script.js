d3ec324689317ab2475f99feb9f80eb0ad82857d
BossFreind1234 <vincentredtube@gmail.com>
2021-03-15 19:21:26 -0400

    added node stuff

---
+++ src/scripts/script.js
@@ -0,0 +1,12 @@
+console.log(`     _______      _________      _____       ______     _
+ / _____ \    |____ ____|    / ___ \     | ____ \   | |
+/ /     \_\       | |       / /   \ \    | |   \ \  | |
+| |               | |      / /     \ \   | |   | |  | |
+\ \______         | |      | |     | |   | |___/ /  | |
+ \______ \        | |      | |     | |   |  ____/   | |
+        \ \       | |      | |     | |   | |        | |
+ _      | |       | |      \ \     / /   | |        |_|
+\ \_____/ /       | |       \ \___/ /    | |         _
+ \_______/        |_|        \_____/     |_|        |_|
+ Do not paste anything here! If you do hackers can steal your info and stuff!
+ If someone is asking you to paste something here and you paste it here, then they will have access to everything, your ip, your password, your cookies, and other stuff.`);
